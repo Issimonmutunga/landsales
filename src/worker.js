d6c88cfe1d0c78010b0028dd1994b4a91c8cabe8
Issimonmutunga <besimonmutunga@gmail.com>
2026-08-30 20:30:18 +0300

    Pass through to assets for SPA fallback

--- src/worker.js
+++ src/worker.js
@@ -1,12 +1,5 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url)
-    const path = url.pathname
-    const last = path.split('/').pop()
-    const hasExt = last.includes('.')
-    if (path !== '/' && !hasExt) {
-      return env.ASSETS.fetch(request.url.replace(url.pathname, '/index.html'))
-    }
     return env.ASSETS.fetch(request)
   }
 }
