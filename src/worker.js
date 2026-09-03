6c181a2b1c4f4f7f23b4ea11c9b5fc49c26db124
Issimonmutunga <besimonmutunga@gmail.com>
2026-09-03 07:33:00 +0300

    Use canonical SPA pass-through worker; rely on not_found_handling to serve index.html deterministically

--- src/worker.js
+++ src/worker.js
@@ -1,11 +1,5 @@
 export default {
   async fetch(request, env) {
-    const asset = await env.ASSETS.fetch(request)
-
-    if (!asset.ok) {
-      return env.ASSETS.fetch(new Request(new URL('/index.html', request.url), request))
-    }
-
-    return asset
+    return env.ASSETS.fetch(request)
   }
 }
