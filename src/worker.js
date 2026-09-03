a5be4015ea73a05d98b0b6e3ff2eafcd4eca6a08
Issimonmutunga <besimonmutunga@gmail.com>
2026-09-03 07:01:15 +0300

    Serve index.html fallback on any non-ok assets response for SPA routes

--- src/worker.js
+++ src/worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     const asset = await env.ASSETS.fetch(request)
 
-    if (!asset.ok && (asset.status === 404 || asset.status === 405)) {
+    if (!asset.ok) {
       return env.ASSETS.fetch(new Request(new URL('/index.html', request.url), request))
     }
 
